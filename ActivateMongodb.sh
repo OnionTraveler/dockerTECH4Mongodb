fbd92714b6211a8da4fe7bc6dc43f70b9e3fcd14
OnionTraveler <47844836+OnionTraveler@users.noreply.github.com>
2019-07-19 17:26:19 +0800

    Update ActivateMongodb.sh

--- ActivateMongodb.sh
+++ ActivateMongodb.sh
@@ -9,7 +9,7 @@ docker exec -i mongodb /bin/bash << ONION
 #========================= (建立資料庫預設存取的目錄位置)
 mkdir -p /data/db
 
-#========================= (啟動mongodb服務) (該服務精靈若要指定存取位置，可改為「bin/mongod --dbpath=/data/db」，其中「/data/db」是芒果資料庫的存取位智預設路徑)
+#========================= (啟動mongodb服務) (該服務精靈若要指定存取位置，可改為「bin/mongod --dbpath=/data/db」，其中「/data/db」是芒果資料庫的存取位置預設路徑)
 cd /usr/mongodb/mongodb; bin/mongod > /dev/null
 ONION
 
